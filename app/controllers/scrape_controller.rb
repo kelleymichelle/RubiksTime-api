2e7bbb9a345e16009ed6653de3f070858ccd77c1
jamievullo <jvullo@protonmail.com>
2020-04-12 14:13:36 -0400

    Added scraping details.

--- app/controllers/scrape_controller.rb
+++ app/controllers/scrape_controller.rb
@@ -8,15 +8,14 @@ class ScrapeController < ApplicationController
         url = 'https://www.worldcubeassociation.org/competitions?utf8=%E2%9C%93&region=_North+America&search=&state=present&year=all+years&from_date=&to_date=&delegate=&display=list'
         doc = Nokogiri::HTML(open(url))
 
-        places = doc.css('.listicle-slide')
-        destinations = places.map do |place|
-                {
-                title: place.css('.listicle-slide-hed-text').text,
-                image: place.css('source')[1].values[0],
-                description: place.css('p').text
-                }
-            # destinations << destination
+        events = doc.css('.list-group-item.not-past')
+        wc_events = events.map do |event|
+            {
+            date: event.css('.date').text.strip,
+            name: event.css('.competition-info').text.strip,
+            location: event.css('.location').text.strip
+            }
         end
-        render json: {results: destinations}
+        render json: {results: wc_events}
     end
 end 
