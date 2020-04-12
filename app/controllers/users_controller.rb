496ea32c877e6b9085e20418e5ab138ce69f4e27
jamievullo <jvullo@protonmail.com>
2020-04-12 10:51:25 -0400

    Added sessions routes and users controller.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,52 @@
+class UsersController < ApplicationController
+
+    def index
+        @users = User.all
+        if @users
+            render json: {
+            users: @users
+            }
+        else
+            render json: {
+            status: 500,
+            errors: ['No Users Found']
+            }
+        end
+    end
+
+    def show
+        @user = User.find(params[:id])
+    if @user
+        render json: {
+            user: @user
+        }
+        else
+        render json: {
+            status: 500,
+            errors: ['User Not Found']
+        }
+        end
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+        login!
+        render json: {
+            status: :created,
+            user: @user
+        }
+        else 
+        render json: {
+            status: 500,
+            errors: @user.errors.full_messages
+        }
+        end
+    end
+
+        private
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
+
+end
